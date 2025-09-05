afb9ce4e2230fa62e73f28eb5984445f2caa9b20
Mongxin <1393907820@qq.com>
2025-09-05 23:58:06 +0800

    Initial the Springboot-framework

---
+++ admin/src/main/java/com/MongxinChan/SaaSshortLink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,13 @@
+package com.MongxinChan.SaaSshortLink.admin;
+
+import org.springfarmework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShortLinkAdminApplication.class,args);
+  }
+
+}
