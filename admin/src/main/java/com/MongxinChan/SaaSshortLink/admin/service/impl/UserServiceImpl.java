fcaa284bb46299e6276002e07a9df1dd6ac012ee
Mongxin <1393907820@qq.com>
2025-09-12 12:41:54 +0800

    feature: 开发短链接后管系统用户登录事件修改为30天

--- admin/src/main/java/com/MongxinChan/SaaSshortLink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/MongxinChan/SaaSshortLink/admin/service/impl/UserServiceImpl.java
@@ -112,7 +112,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
         stringRedisTemplate.opsForHash()
                 .put("login_" + requestParam.getUsername(), uuid, JSON.toJSONString(userDO));
-        stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.MINUTES);
+        stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.DAYS);
         return new UserLoginRespDTO(uuid);
     }
 
