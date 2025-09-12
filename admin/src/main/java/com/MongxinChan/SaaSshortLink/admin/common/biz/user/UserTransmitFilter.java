7551c48c39978e64c58e0d486f237e53bd29ad3b
Mongxin <1393907820@qq.com>
2025-09-12 12:47:02 +0800

    feature: 开发短链接后管系统用户上下文封装

---
+++ admin/src/main/java/com/MongxinChan/SaaSshortLink/admin/common/biz/user/UserTransmitFilter.java
@@ -0,0 +1,44 @@
+package com.MongxinChan.SaaSshortLink.admin.common.biz.user;
+
+import com.alibaba.fastjson2.JSON;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+/**
+ * 用户信息传输过滤器
+ */
+public class UserTransmitFilter implements Filter {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public UserTransmitFilter(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+            FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String username = httpServletRequest.getHeader("userName");
+        String token = httpServletRequest.getHeader("token");
+        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+        if (userInfoJsonStr != null) {
+
+            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(),
+                    UserInfoDTO.class);
+            UserContext.setUser(userInfoDTO);
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
+    }
+}
