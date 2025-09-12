07ee348db434a3e235f903f3279fa18fa398c9f3
Mongxin <1393907820@qq.com>
2025-09-13 00:41:50 +0800

    feature: 增强短链接项目创建短链接功能

--- project/src/main/java/com/MongxinChan/SaaSshortLink/project/dao/entity/ShortLinkDO.java
+++ project/src/main/java/com/MongxinChan/SaaSshortLink/project/dao/entity/ShortLinkDO.java
@@ -4,14 +4,20 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 /**
  * 短链接实体
  */
 @Data
+@Builder
 @TableName("tLink")
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShortLinkDO {
 
     /**
--- project/src/main/java/com/MongxinChan/SaaSshortLink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/MongxinChan/SaaSshortLink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,6 +1,6 @@
 package com.MongxinChan.SaaSshortLink.project.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.text.StrBuilder;
 import com.MongxinChan.SaaSshortLink.project.common.convention.exception.ServiceException;
 import com.MongxinChan.SaaSshortLink.project.dao.entity.ShortLinkDO;
 import com.MongxinChan.SaaSshortLink.project.dao.mapper.ShortLinkMapper;
@@ -8,6 +8,8 @@ import com.MongxinChan.SaaSshortLink.project.dto.req.ShortLinkCreateReqDTO;
 import com.MongxinChan.SaaSshortLink.project.dto.resp.ShortLinkCreateRespDTO;
 import com.MongxinChan.SaaSshortLink.project.service.ShortLinkService;
 import com.MongxinChan.SaaSshortLink.project.toolkit.HashUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -43,23 +45,43 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
         }
 
         String originUrl = requestParam.getOriginUrl();
+        originUrl += System.currentTimeMillis();
+        shortURI = HashUtil.hashToBase62(originUrl);
         return HashUtil.hashToBase62(originUrl);
     }
 
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
         String shortLinkSuffix = generatedSuffix(requestParam);
-        String fullShortUrl = requestParam.getDomain() + "/" + shortLinkSuffix;
-        ShortLinkDO shortLinkDO = BeanUtil.toBean(requestParam, ShortLinkDO.class);
-        shortLinkDO.setFullShortUrl(requestParam.getDomain() + "/" + shortLinkSuffix);
+        String fullShortUrl = StrBuilder.create(requestParam.getDomain())
+                .append("/")
+                .append(shortLinkSuffix)
+                .toString();
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .domain(requestParam.getDomain())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .createdType(requestParam.getCreatedType())
+                .validDateType(requestParam.getValidDateType())
+                .validDate(requestParam.getValidDate())
+                .describe(requestParam.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .enableStatus(0)
+                .fullShortUrl(fullShortUrl)
+                .build();
         try {
             baseMapper.insert(shortLinkDO);
         } catch (DuplicateKeyException ex) {
             // TODO 已经误判的短链接如何处理
             // 1.短链接真实存在缓存中
             // 2.短链接不一定在缓存中
-            log.warn("短链接{} 重复入库", fullShortUrl);
-            throw new ServiceException("短链接生成重复");
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO hasShortLinkDO = baseMapper.selectOne(queryWrapper);
+            if (hasShortLinkDO != null) {
+                log.warn("短链接{} 重复入库", fullShortUrl);
+                throw new ServiceException("短链接生成重复");
+            }
         }
         shortUriCreateCachePenetrationBloomFilter.add(shortLinkSuffix);
         return ShortLinkCreateRespDTO.builder()
